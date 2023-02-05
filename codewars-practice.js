3812cc147e70c0471165430443587e6e34674de2
Muhammad Shahryar <Muhammdshahryar19@hotmail.com>
2023-02-05 21:10:15 +0000

    Code practice

--- codewars-practice.js
+++ codewars-practice.js
@@ -17,3 +17,10 @@ function reverseString(str) {
 function wordLengths(str) {
   return str.split(' ').map(word => word.length);
 }
+
+
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers for javascript
+
+function evenOrOdd(num) {
+  return num % 2 === 0 ? "Even" : "Odd";
+}
